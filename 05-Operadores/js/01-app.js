a86b42564b8d5fed4099a512a656da88e9717231
Jorge Yael <jorge.yael333@gmail.com>
2023-12-06 09:31:03 -0600

    05- operadores finalizado

---
+++ 05-Operadores/js/01-app.js
@@ -0,0 +1,8 @@
+const num1 = 10;
+const num2 = 20;
+
+// Operador mayor que
+console.log( num1 > num1); // false
+
+// Operador menor que
+console.log( num1 < num2);
---
+++ 05-Operadores/js/03-app.js
@@ -0,0 +1,13 @@
+let numero
+
+// UNDEFINED
+console.log(numero); // no tiene ningun valor definido
+console.log(  typeof numero );
+
+// NULL
+numero = null
+console.log(numero);
+console.log( typeof numero );
+
+console.log( null === undefined ); // false
+console.log( null == undefined ); // true
